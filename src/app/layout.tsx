db595825b3c387a65b18f74c4bebb844a73ef44e
AbSrDUO1 <163032126+AbSrDUO1@users.noreply.github.com>
2025-10-22 20:27:53 +0530

    Update project files

---
+++ src/app/layout.tsx
@@ -0,0 +1,84 @@
+import type { Metadata } from "next";
+import {
+  Inter_Tight,
+  Playfair_Display,
+  Roboto,
+  Open_Sans,
+  Lato,
+  Montserrat,
+  Poppins,
+  Raleway,
+  Ubuntu,
+  Nunito
+} from "next/font/google";
+import "./globals.css";
+import { PostHogWrapper } from "@/components/PostHogWrapper";
+import AuroraBackground from "@/components/background/AuroraBackground";
+import Tag from "@/tag/Tag";
+
+const interTight = Inter_Tight({
+  variable: "--font-inter-tight",
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
+
+const playfairDisplay = Playfair_Display({
+  variable: "--font-playfair",
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700", "800", "900"],
+});
+
+const roboto = Roboto({
+  variable: "--font-roboto",
+  subsets: ["latin"],
+  weight: ["100", "300", "400", "500", "700", "900"],
+});
+
+const openSans = Open_Sans({
+  variable: "--font-open-sans",
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700", "800"],
+});
+
+const lato = Lato({
+  variable: "--font-lato",
+  subsets: ["latin"],
+  weight: ["100", "300", "400", "700", "900"],
+});
+
+const montserrat = Montserrat({
+  variable: "--font-montserrat",
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
+
+const poppins = Poppins({
+  variable: "--font-poppins",
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
+
+const raleway = Raleway({
+  variable: "--font-raleway",
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
+
+const ubuntu = Ubuntu({
+  variable: "--font-ubuntu",
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "700"],
+});
+
+const nunito = Nunito({
+  variable: "--font-nunito",
+  subsets: ["latin"],
+  weight: ["200", "300", "400", "500", "600", "700", "800", "900"],
+});
+
+export const metadata: Metadata = {
+  title: "Pizza Perfection - Authentic Italian Pizza",
+  description: "Handcrafted pizzas made with fresh ingredients and baked to perfection in our traditional wood-fired oven. Experience the authentic taste of Italy."
+};
+
+export default function
